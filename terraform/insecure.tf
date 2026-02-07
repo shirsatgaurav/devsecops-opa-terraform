9d62c16adcee32bf6615ade446fd1a0630ce7015
shirsatgaurav <shirsatgaurav92@gmail.com>
2026-02-07 06:42:49 +0530

    Harden S3 encryption and restrict SG access

--- terraform/insecure.tf
+++ terraform/insecure.tf
@@ -2,18 +2,26 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "public_bucket" {
-  bucket = "devsecops-demo-bucket-123"
-   acl    = "public-read"
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "opa-secure-demo-bucket-12345"
+  acl    = "private"
+
+ server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
-resource "aws_security_group" "open_sg" {
-  name = "open-sg"
+resource "aws_security_group" "secure_sg" {
+  name = "secure-sg"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["42.108.236.147/32"]
   }
 }
